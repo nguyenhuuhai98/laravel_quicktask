3c5263930913e93c41a1b33fba4b571e2601a537
Huu Hai <nguyenhuuhai98@gmail.com>
2019-10-14 10:07:26 +0700

    Task controller + Auth controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks', 'TaskController@index')->name('all.tasks');
