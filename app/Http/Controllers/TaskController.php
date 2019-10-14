cae755a566b15e937c96987e3568e5966e50c105
Huu Hai <nguyenhuuhai98@gmail.com>
2019-10-14 11:53:44 +0700

    controllers

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Repositories\TaskRepository;
+
+class TaskController extends Controller
+{
+    protected $tasks;
+
+    public function __construct(TaskRepository $tasks)
+    {
+        $this->middleware('auth');
+
+        $this->tasks = $tasks;
+    }
+
+    public function index(Request $request)
+    {
+        $task = $request->user()->tasks()->get();
+
+        return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
+    }
+
+    public function destroy()
+    {
+
+    }
+}
