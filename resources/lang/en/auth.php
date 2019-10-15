22eb0c95af537771e197ce0afe705c40d0bf0e3c
Huu Hai <nguyenhuuhai98@gmail.com>
2019-10-15 14:12:04 +0700

    Make auth

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -16,4 +16,31 @@ return [
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
+    //login & register
+    'log' => 'You are login!',
+    'dashboard' => 'Dashboard',
+    'login' => 'Log in',
+    'register' => 'Register',
+    'email' => 'E-Mail Address',
+    'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+    'remember' => 'Remember Me',
+    'forgot_password' => 'Forgot Your Password',
+    'name' => 'Name',
+
+    //verify
+    'verify_email' => 'Verify Your Email Address',
+    'verification_link' => 'A fresh verification link has been sent to your email address.',
+    'check_your_email' => 'Before proceeding, please check your email for a verification link.',
+    'did_not_receive_the_email' => 'If you did not receive the email',
+    'request_another' => 'click here to request another',
+
+    //confirm password
+    'please_confirm_password' => 'Please confirm your password before continuing.',
+
+    //reset password
+    'reset_password' => 'Reset password',
+    'send_password_reset_link' => 'Send Password Reset Link',
+
+
 ];
